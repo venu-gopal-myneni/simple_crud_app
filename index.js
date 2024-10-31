06b556331ccdd9b2e38081367883d4ec9a80596d
vmyneni <mailvenumyneni@gmail.com>
2024-10-31 20:34:08 +0530

    EoD

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Student = require('./models/student.model.js');
 const app = express()
 
 app.use(express.json()); //middleware to send json based POST
+app.use(express.urlencoded({extended:false})); //middleware to send form based POST
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => 
@@ -20,15 +21,73 @@ app.get("/", (req, res) => {
     res.send("Hello Wizard from Hogwarts !!");
 })
 
-app.post("/hogwarts/students", async (req,res) => {
+
+// get all students
+app.get("/hogwarts/students", async (req,res) => {
     try {
-        await Student.create(req.body);
+        const student = await Student.find({});
+        res.status(200).json(student)
 
     } catch (error) {
         res.status(500).json({message:error.message});
     }
-    console.log(req.body);
-    res.send(req.body);
 })
 
+// get 1 student by id 
+app.get("/hogwarts/students/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const student = await Student.findById(id);
+        res.status(200).json(student)
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
+
+// add 1 student
+app.post("/hogwarts/students", async (req,res) => {
+    try {
+        const student = await Student.create(req.body);
+        res.status(200).json(student)
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
+
+// update 1 student by id
+app.put("/hogwarts/students/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const student = await Student.findByIdAndUpdate(id, req.body);
+
+        if (!student) {
+            return res.status(404).json({message:"Product Not Found"})
+        }
+        const updatedStudent = await Student.findById(id);
+        res.status(200).json(updatedStudent)
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
+
+// Delete 1 student by id
+app.delete("/hogwarts/students/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const student = await Student.findByIdAndDelete(id);
+
+        if (!student) {
+            return res.status(404).json({message:"Product Not Found"})
+        }
+        res.status(200).json({message:"Product Deleted"})
+        res.status(200).json(student)
+
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
 
